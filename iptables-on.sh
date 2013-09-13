86d167497346df965d99a4245c0cb5d32f3078a6
Thiago Zilli <thiago.zilli@gmail.com>
2013-09-13 12:30:20 -0300

    ajuste no iptables para todos as portas ficarem visiveis a maquina bridge

--- iptables-on.sh
+++ iptables-on.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#Config para sua vm ficar transprente a maquina ponte.
+sudo iptables -t nat -A POSTROUTING -o vboxnet1 -j MASQUERADE
+sudo iptables -A FORWARD -i vboxnet1 -o eth0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i eth0 -o vboxnet1 -j ACCEPT
+
 #Hbase ports
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 60000 -j DNAT --to 10.0.1.110:60000
 sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 60010 -j DNAT --to 10.0.1.110:60010
